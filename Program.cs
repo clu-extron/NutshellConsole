ac709f15282c4722db8446e520db911bed3a03ce
Chaoran Lu <clu@extron.com>
2023-11-28 21:51:53 -0800

    Yield Return

--- Program.cs
+++ Program.cs
@@ -1,8 +1,57 @@
 ﻿using NutshellConsole.Ch7;
 
-var bank = new Bank();
-
-foreach (var c in bank)
+static IEnumerable<Customer> GetCustomers(int count)
 {
-    Console.WriteLine(c.Name);
+    var customers = new List<Customer>();
+    for (int i = 0; i < count; i++)
+    {
+        customers.Add(new Customer(i, $"Alex{i}", $"Location{i}"));
+    }
+    return customers;
+}
+
+static IEnumerable<Customer> GetCustomersYield(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        yield return new Customer(i, $"Alex{i}", $"Location{i}");
+    }
+}
+
+//var customers = GetCustomers(1000000);
+//foreach (var c in customers)
+//{
+//    if (c.Id < 1000)
+//    {
+//        Console.WriteLine($"Customer ID {c.Id}, Customer Name: {c.Name}");
+//    }
+//    else
+//    {
+//        break;
+//    }
+//}
+
+//var customers = GetCustomersYield(1000000);
+//foreach (var c in customers)
+//{
+//    if (c.Id < 3)
+//    {
+//        Console.WriteLine($"Customer ID {c.Id}, Customer Name: {c.Name}");
+//    }
+//    else
+//    {
+//        break;
+//    }
+//}
+
+static IEnumerable<int> CreatEnumerable()
+{
+    yield return 3;
+    yield return 2;
+    yield return 1;
+}
+
+foreach (var item in CreatEnumerable())
+{
+    Console.WriteLine(item);
 }
