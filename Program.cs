e198f0911eb40820853d5d835ee6ea770cbaf08f
Chaoran Lu <clu@extron.com>
2023-11-28 22:29:15 -0800

    Dictionary and HashTable

--- Program.cs
+++ Program.cs
@@ -1,5 +1,33 @@
-﻿using BenchmarkDotNet.Running;
-using NutshellConsole;
+﻿using NutshellConsole.Ch7;
+using System.Collections;
 
-var summary = BenchmarkRunner.Run<BenchmarkTester>();
+static Dictionary<int, Customer> GetCustomersDictionary(int count)
+{
+    var customers = new Dictionary<int, Customer>();
+    for (int i = 0; i < count; i++)
+    {
+        customers.Add(i, new Customer(i, $"Alex{i}", $"Location{i}"));
+    }
+    return customers;
+}
+
+static Hashtable GetCustomersHashTable(int count)
+{
+    var customers = new Hashtable();
+    for (int i = 0; i < count; i++)
+    {
+        customers.Add(i, new Customer(i, $"Alex{i}", $"Location{i}"));
+    }
+    return customers;
+}
+
+//var customers = GetCustomersDictionary(1000000);
+//var target = customers[99999];
+//Console.WriteLine($"Customer ID {target.Id}, Customer Name: {target.Name}");
+
+// HashTable
+var customers = GetCustomersHashTable(1000000);
+var target = (Customer)customers[99999];
+Console.WriteLine($"Customer ID {target.Id}, Customer Name: {target.Name}");
+
 Console.Read();
