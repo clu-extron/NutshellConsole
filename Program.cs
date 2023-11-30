56ccc02029ef6767a921803d2d3fac0f67448408
Chaoran Lu <clu@extron.com>
2023-11-30 12:54:38 -0800

    Thread Pool

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿Thread thread = new Thread(PrintHello);
-
-// Switch to back-end thread
-//thread.IsBackground = true;
-thread.Start();
-
-Console.WriteLine("End of Main Program");
-
-void PrintHello(object? obj)
+﻿int count = 0;
+for (int i = 0; i < 100; i++)
 {
-    while (true)
+    //var t = new Thread(() =>
+    //{
+    //    Console.WriteLine($"recycle times {i} thread id {Thread.CurrentThread.ManagedThreadId}");
+    //});
+    //t.Start();
+
+    // Using Thread Pool
+    ThreadPool.QueueUserWorkItem((o) =>
     {
-        Thread.Sleep(1000);
-        Console.WriteLine("Hello from PrintHello");
-    }
+        count++;
+        Console.WriteLine($"Thread ID {Thread.CurrentThread.ManagedThreadId}");
+    });
 }
+
+// Give the thread pool enough time to finish all the threads
+//Thread.Sleep(5000);
+
+Console.WriteLine($"End of Main Program, there are {count} threads in operation");
