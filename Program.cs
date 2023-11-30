664d27563ee4048a05e19e8127bd826de2b03f8d
Chaoran Lu <clu@extron.com>
2023-11-30 12:57:39 -0800

    comment on how to use thread pool

--- Program.cs
+++ Program.cs
@@ -19,3 +19,6 @@ for (int i = 0; i < 100; i++)
 //Thread.Sleep(5000);
 
 Console.WriteLine($"End of Main Program, there are {count} threads in operation");
+
+// 对于重要且并发量小的线程，我们需要手动来创建和管理
+// 对于并发量大, 而又不那么重要的线程, 我们最好托管在线程池中
