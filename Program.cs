998afc14950ff21d83f47a353bc022ce2694b81e
Chaoran Lu <clu@extron.com>
2023-11-30 13:08:57 -0800

    cancellation token

--- Program.cs
+++ Program.cs
@@ -1,24 +1,23 @@
-﻿int count = 0;
-for (int i = 0; i < 100; i++)
+﻿CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+Thread thread = new Thread(()=>
 {
-    //var t = new Thread(() =>
-    //{
-    //    Console.WriteLine($"recycle times {i} thread id {Thread.CurrentThread.ManagedThreadId}");
-    //});
-    //t.Start();
+    PrintHello(tokenSource.Token);
+});
+thread.Start();
 
-    // Using Thread Pool
-    ThreadPool.QueueUserWorkItem((o) =>
+// Mock Download File
+Thread.Sleep(5000);
+
+// Shut down child thread
+tokenSource.Cancel();
+Console.WriteLine("Exit Main Program");
+
+void PrintHello(CancellationToken token)
+{
+    while (!token.IsCancellationRequested)
     {
-        count++;
-        Console.WriteLine($"Thread ID {Thread.CurrentThread.ManagedThreadId}");
-    });
+        Thread.Sleep(1000);
+        Console.WriteLine("Hello from PrintHello");
+    }
 }
-
-// Give the thread pool enough time to finish all the threads
-//Thread.Sleep(5000);
-
-Console.WriteLine($"End of Main Program, there are {count} threads in operation");
-
-// 对于重要且并发量小的线程，我们需要手动来创建和管理
-// 对于并发量大, 而又不那么重要的线程, 我们最好托管在线程池中
