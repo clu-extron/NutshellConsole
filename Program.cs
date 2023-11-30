7803c2933c4e992fb0a49663ce561e548c77b82b
Chaoran Lu <clu@extron.com>
2023-11-30 13:31:35 -0800

    Join and IsAlive

--- Program.cs
+++ Program.cs
@@ -1,23 +1,36 @@
 ﻿CancellationTokenSource tokenSource = new CancellationTokenSource();
 
-Thread thread = new Thread(()=>
-{
-    PrintHello(tokenSource.Token);
-});
+Thread thread = new Thread(PrintHello);
 thread.Start();
 
-// Mock Download File
-Thread.Sleep(5000);
+//// 将主线程和子线程绑定起来, 主线程会被挂起,
+//// 等待子线程执行完毕后, 主线程会接收到子线程执行完毕的通知, 继续执行
+//thread.Join();
 
-// Shut down child thread
-tokenSource.Cancel();
+bool check = true;
+while (check)
+{
+    if (thread.IsAlive)
+    {
+        Console.WriteLine("child tread RUNNING");
+        Thread.Sleep(100);
+    }
+    else
+    {
+        Console.WriteLine("child thread COMPLETE");
+        check = false;
+    }
+}
+
+
 Console.WriteLine("Exit Main Program");
 
-void PrintHello(CancellationToken token)
+void PrintHello(object obj)
 {
-    while (!token.IsCancellationRequested)
+    int i = 0;
+    while (i++ < 10)
     {
-        Thread.Sleep(1000);
+        Thread.Sleep(new Random().Next(100, 1000));
         Console.WriteLine("Hello from PrintHello");
     }
 }
