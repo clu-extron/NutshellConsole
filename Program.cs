feeef58b4883118ee3f699d9ece71f1d6d2be995
Chaoran Lu <clu@extron.com>
2023-11-30 22:33:26 -0800

    Async Await

--- Program.cs
+++ Program.cs
@@ -8,68 +8,39 @@
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Calculate();
+            CalculateAsync();
             Console.Read();
         }
 
-        static void Calculate()
+        static async void CalculateAsync()
         {
-            // Task -> 异步
-            // Thread -> 线程
-            //Calculate1();
-            //Calculate2();
-            //Calculate3();
-
-            var task1 = Task.Run(() =>
-            {
-                return Calculate1();
-            });
-
-            var awaiter1 = task1.GetAwaiter();
-
-            awaiter1.OnCompleted(() =>
-            {
-                var result1 = awaiter1.GetResult();
-
-                var task2 = Task.Run(() =>
-                {
-                    return Calculate2(result1);
-                });
-
-                var awaiter2 = task2.GetAwaiter();
-
-                awaiter2.OnCompleted(() =>
-                {
-                    var result2 = awaiter2.GetResult();
-                    var task3 = Task.Run(() =>
-                    {
-                        Calculate3(result1, result2);
-                    });
-                });
-            });
+            var result1 = await Calculate1Async();
+            var result2 = await Calculate2Async(result1);
+            var result = await Calculate3Async(result1, result2);
+            Console.WriteLine(result);
         }
 
-        static int Calculate1()
+        static async Task<int> Calculate1Async()
         {
             var result = 3;
             Console.WriteLine($"Calculate1； {result}");
-            Thread.Sleep(2000);
+            await Task.Delay(3000);
             return result;
         }
 
-        static int Calculate2(int a)
+        static async Task<int> Calculate2Async(int a)
         {
-            var result = a + 4;
+            var result = a + 3;
             Console.WriteLine($"Calculate2； {result}");
-            Thread.Sleep(3000);
+            await Task.Delay(2000);
             return result;
         }
 
-        static int Calculate3(int a, int b)
+        static async Task<int> Calculate3Async(int a, int b)
         {
             var result = a + b;
             Console.WriteLine($"Calculate3； {result}");
-            Thread.Sleep(1000);
+            await Task.Delay(1000);
             return result;
         }
     }
