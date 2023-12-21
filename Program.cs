95fc241b5e681de4164cac7b866d1187654ccfa6
Chaoran Lu <clu@extron.com>
2023-12-21 15:22:34 -0800

    summary for next work

--- Program.cs
+++ Program.cs
@@ -6,24 +6,16 @@ namespace NutshellConsole
     class Program
     {
         /// <summary>
-        /// 1.一个类只能有一个终结器。
-        /// 2.不能继承或重载终结器。
-        /// 3.不能手动调用终结器。
-        /// 4.终结器不使用修饰符或参数。
+        /// Models: map data from our database to application (Source Mapping)
+        /// Namespaces: Where the code lives
+        /// Database Connection(Dapper & Entity Framework)
+        /// Config: connections string
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            DoSomething();
-            GC.WaitForPendingFinalizers();
-            GC.Collect();
-
-            Console.WriteLine("Program terminated.");
+            Console.WriteLine("Hello C# programmer!");
         }
 
-        static void DoSomething()
-        {
-            new SubTest2();
-        }
     }
 }
